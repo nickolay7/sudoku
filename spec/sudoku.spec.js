f878e267abae46998cf19fcb5968c9c70c74444b
nickolay7 <shkelal50@gmail.com>
2022-10-28 10:17:48 +0300

    refactored

--- spec/sudoku.spec.js
+++ spec/sudoku.spec.js
@@ -1,10 +1,10 @@
-const sudoku = require('../sudoku.js')
+const { solve, isSolved } = require('../sudoku.js')
 
 describe('Basic test', () => {
-  const str = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--';
+  const str = '--7--8------2---6-65--79----7----3-5-83---67-2-1----8----71--38-2---5------4--2--';
 
   it('Should return an array with 9 subarrays with length equal to 9', () => {
-    expect(solve(str)).toEqual(Array.from(str));
-    expect(solve(isSolved(str))).toBeTruthy();
+    console.log(solve(str));
+    expect(isSolved(solve(str))).toBeTruthy();
   });
 });
--- sudoku.js
+++ sudoku.js
@@ -1,19 +1,110 @@
-
 // const scores = fs.readFileSync('./data/scores.txt', 'utf-8');
 // Takes a board as a string in the format
 // you see in the puzzle file. Returns
 // something representing a board after
 // your solver has tried to solve it.
 // How you represent your board is up to you!
-function solve(boardString) {  
+function solve(boardString) {
   let regExp = /(.{9})/g;
   let arrBoard = boardString.match(regExp);
   const resultBoard = arrBoard.map((element) => {
-    return element.split('')
+    return element.split('');
   });
 
-return   resultBoard
+  const coll = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
+  // iterate board to get empty values cords
+  const getEmptyValuesCords = () => {
+    const emptyValuesCords = [];
+    for (let x = 0; x < 9; x += 1) {
+      for (let y = 0; y < 9; y += 1) {
+        if (resultBoard[x][y] === '-') {
+          emptyValuesCords.push([x, y]);
+        }
+      }
+    }
+    return emptyValuesCords;
+  };
+
+
+  const getValuesFromX = (xCord) => {
+    return resultBoard[xCord].filter((el) => el !== '-');
+  };
+
+  const getValuesFromY = (yCord) => {
+    const res = [];
+    for (let x = 0; x < 9; x += 1) {
+      if (resultBoard[x][yCord] !== '-') {
+        res.push(resultBoard[x][yCord]);
+      }
+    }
+    return res;
+  };
+
+  const getValuesFromBlock = (x, y) => {
+    let res = [];
+    const [first, second, third] = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+    const blockIndexes = [];
+    if (first.includes(x)) {
+      blockIndexes.push(first);
+    }
+    if (second.includes(x)) {
+      blockIndexes.push(second);
+    }
+    if (third.includes(x)) {
+      blockIndexes.push(third);
+    }
+    if (first.includes(y)) {
+      blockIndexes.push(first);
+    }
+    if (second.includes(y)) {
+      blockIndexes.push(second);
+    }
+    if (third.includes(y)) {
+      blockIndexes.push(third);
+    }
+    const [xIndexes, yIndexes] = blockIndexes;
+    for (let x = xIndexes[0]; x <= xIndexes[2] ; x += 1) {
+      for (let y = yIndexes[0]; y <= yIndexes[2]; y += 1) {
+        if (resultBoard[x][y] !== '-') {
+          res.push(resultBoard[x][y]);
+        }
+      }
+    }
+
+    return res;
+  };
+
+  const go = () => {
+    const emptyValuesCords = getEmptyValuesCords(resultBoard);
+    for (const [x, y] of emptyValuesCords) {
+      const rowValues = getValuesFromX(x);
+      const columnValues =getValuesFromY(y);
+      const blockValues = getValuesFromBlock(x, y);
+      for (const num of coll) {
+        if (!rowValues.includes(num) && !columnValues.includes(num) && !blockValues.includes(num)) {
+          resultBoard[x][y] = num;
+          break;
+        }
+      }
+    }
+    return resultBoard;
+  };
+
+  return go();
+  // [
+  //   [ 0, 1 ], [ 0, 4 ], [ 0, 6 ], [ 0, 7 ],
+  //   [ 0, 8 ], [ 1, 0 ], [ 1, 2 ], [ 1, 3 ],
+  //   [ 1, 7 ], [ 2, 1 ], [ 2, 2 ], [ 2, 4 ],
+  //   [ 2, 5 ], [ 3, 0 ], [ 3, 3 ], [ 3, 4 ],
+  //   [ 3, 7 ], [ 4, 3 ], [ 4, 6 ], [ 4, 8 ],
+  //   [ 5, 0 ], [ 5, 1 ], [ 5, 2 ], [ 5, 3 ],
+  //   [ 5, 6 ], [ 5, 8 ], [ 6, 0 ], [ 6, 1 ],
+  //   [ 6, 4 ], [ 6, 5 ], [ 6, 6 ], [ 6, 8 ],
+  //   [ 7, 2 ], [ 7, 3 ], [ 7, 5 ], [ 7, 7 ],
+  //   [ 8, 1 ], [ 8, 2 ], [ 8, 4 ], [ 8, 7 ],
+  //   [ 8, 8 ]
+  // ]
 }
 
 // Returns a boolean indicating whether
@@ -36,7 +127,11 @@ function prettyBoard(board) {
 
 // Exports all the functions to use them in another file.
 module.exports = {
-	solve: solve,
-	isSolved: isSolved,
-	prettyBoard: prettyBoard
+  solve: solve,
+  isSolved: isSolved,
+  prettyBoard: prettyBoard
 }
+
+const str = '--7--8------2---6-65--79----7----3-5-83---67-2-1----8----71--38-2---5------4--2--';
+
+solve(str);
